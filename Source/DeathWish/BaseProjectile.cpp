1d5727881d4563c106f5f07f0d2a6bd11b09d398
VenomSolo <karolikjakub@gmail.com>
2017-10-09 10:50:22 +0200

    Added trail color change

--- Source/DeathWish/BaseProjectile.cpp
+++ Source/DeathWish/BaseProjectile.cpp
@@ -19,6 +19,16 @@ ABaseProjectile::ABaseProjectile()
 	mesh->AttachToComponent(collision, FAttachmentTransformRules::KeepWorldTransform, "mesh");
 	ribbon = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ribbon"));
 	ribbon->AttachToComponent(mesh, FAttachmentTransformRules::KeepWorldTransform, "ribbon");
+	switch(team){
+		case ETeamEnum::TE_Red:
+			setTrailColor(FLinearColor(225.0f, 25.0f, 25.0f, 1.0f));
+		case ETeamEnum::TE_Blue:
+			setTrailColor(FLinearColor(25.0f, 100.0f, 225.0f, 1.0f));
+		case ETeamEnum::TE_Yellow:
+			setTrailColor(FLinearColor(235.0f, 235.0f, 40.0f, 1.0f));
+		case ETeamEnum::TE_Green:
+			setTrailColor(FLinearColor(25.0f, 225.0f, 25.0f, 1.0f));
+	}
 	movement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("movement"));
 
 }
@@ -33,6 +43,18 @@ void ABaseProjectile::onHit_Implementation(AActor * SelfActor, AActor * OtherAct
 		hitPlayer->takeDamageSRPC(damage);
 		Destroy();
 	}
+}
+
+bool ABaseProjectile::setTrailColor_Validate(FLinearColor newColor){
+	if (ribbon != NULL){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+void ABaseProjectile::setTrailColor_Implementation(FLinearColor newColor){
+	ribbon->SetColorParameter("trailColor", newColor);
 }
 
 // Called when the game starts or when spawned
