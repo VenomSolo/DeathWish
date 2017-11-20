6ed67a0d258958978d8221187485fc146ebc2fd3
VenomSolo <karolikjakub@gmail.com>
2017-11-20 11:13:41 +0100

    New Enums

--- Source/DeathWish/EnumsAndStructs.h
+++ Source/DeathWish/EnumsAndStructs.h
@@ -8,10 +8,37 @@
 UENUM(BlueprintType)
 enum class ETeamEnum : uint8
 {
-	TE_Red 	UMETA(DisplayName = "Red"),
+	TE_Red 		UMETA(DisplayName = "Red"),
 	TE_Blue 	UMETA(DisplayName = "Blue"),
 	TE_Yellow	UMETA(DisplayName = "Yellow"),
 	TE_Green	UMETA(DisplayName = "Green"),
+};
+
+UENUM(BlueprintType)
+enum class EDebuffEnum : uint8
+{
+	DE_Slow		UMETA(DisplayName = "Slow"),
+	DE_Vulnerability 	UMETA(DisplayName = "Vulnerability"),
+	DE_Recoil	UMETA(DisplayName = "Recoil"),
+};
+
+UENUM(BlueprintType)
+enum class EEffectEnum : uint8
+{
+	EE_Wounded 	UMETA(DisplayName = "Wounded"),
+	EE_Stunned	UMETA(DisplayName = "Stunned"),
+	EE_Asleep	UMETA(DisplayName = "Asleep"),
+	EE_Paralyzed	UMETA(DisplayName = "Paralyzed"),
+};
+
+UENUM(BlueprintType)
+enum class EBulletTypeEnum : uint8
+{
+	BTE_Normal 	UMETA(DisplayName = "Normal"),
+	BTE_Wounding 	UMETA(DisplayName = "Wounding"),
+	BTE_Sleeping	UMETA(DisplayName = "Sleeping"),
+	BTE_Penetrating	UMETA(DisplayName = ""),
+	BTE_AntiRicochet	UMETA(DisplayName = "Green"),
 };
 
 USTRUCT(BlueprintType)
